90a3be4bf1d512d0b925b30bb08b97e875b58e73
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2019-12-12 19:31:13 -0800

    Number guessing game
    guessing game built in C#.  Uses basics and while/if/else loop.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 // Namespace
+// Not DRY code - expanded to fully understand all components
 namespace CSharpTutorial
 {
 // Main Class
@@ -20,30 +21,21 @@ namespace CSharpTutorial
             Console.WriteLine("{0} is {1}", name, age);
             Console.Beep();
 
-            // set app vars
-            string appName = "Number Guesser";
-            string appVersion = "1.0.0";
-            string appAuthor = "Ryan Pitts";
+            GetAppInfo(); // Run GetAppInfo function
 
-            //change text color
-            Console.ForegroundColor = ConsoleColor.DarkGray;
+            GreetUser(); // Ask for users name and greet
 
-            //Write out app info
-            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
-
-            //reset back to the default white color
-            Console.ResetColor();
-
-            // Ask the users name
-            Console.WriteLine("What is your name");
-
-            // Get user input
-            string inputName = Console.ReadLine();
-
-            Console.WriteLine("Hello {0}, lets play a game", inputName);
-
+            while (true) { 
             // Set correct number
-            int correctNumber = 7;
+            //int correctNumber = 7;
+
+            // Create a new random object
+            // Random object is available for use
+            Random random = new Random();
+
+            //Generates a random number
+            // 
+            int correctNumber = random.Next(1,10);
 
             // Init guess var
             int guess = 0;
@@ -54,15 +46,35 @@ namespace CSharpTutorial
             // While guess is not correct
             // if the number doesnt match we want to loop until user gets it right
 
-            while (guess !=correctNumber)
-                //Get users input
+            while (guess != correctNumber)
+            //Get users input
             {
                 string input = Console.ReadLine();
 
+                // Make sure its a number
+                if (!int.TryParse(input, out guess))
+                {
+
+                    //change text color
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    //Write out app info
+                    Console.WriteLine("Please enter a number");
+
+                    //reset back to the default white color
+                    Console.ResetColor();
+
+                    // Keep going
+                    continue;
+
+                }
+
                 // Cast to int and put in guess
+
                 guess = Int32.Parse(input);
 
                 // Match guess to correct number
+
                 if (guess != correctNumber)
                 {
                     //change text color
@@ -90,6 +102,54 @@ namespace CSharpTutorial
             Console.ResetColor();
 
 
+
+            // Ask to play again
+            Console.WriteLine("Play Again? [Y or N]");
+
+            // Get answer
+            string answer = Console.ReadLine().ToUpper();
+
+            if (answer == "Y")
+            {
+                continue;
+            }
+            else if (answer == "N")
+            {
+                return;
+            }
+            else
+            {
+                return;
+            }
+         }
+       }
+
+        static void GetAppInfo()
+        {
+            // set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Ryan Pitts";
+
+            //change text color
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            //Write out app info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            //reset back to the default white color
+            Console.ResetColor();
+        }
+
+        static void GreetUser()
+        {
+            // Ask the users name
+            Console.WriteLine("What is your name");
+
+            // Get user input
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, lets play a game", inputName);
         }
     }
 }
